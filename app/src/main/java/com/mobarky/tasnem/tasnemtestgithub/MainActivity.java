b108618129d483dc1e9e4b7ae6665c8b1acc6e47
Noor21afeefy <sweetnora838@gmail.com>
2017-09-13 10:52:31 +0300

    hjhiu

--- app/src/main/java/com/mobarky/tasnem/tasnemtestgithub/MainActivity.java
+++ app/src/main/java/com/mobarky/tasnem/tasnemtestgithub/MainActivity.java
@@ -2,6 +2,7 @@ package com.mobarky.tasnem.tasnemtestgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
 }
