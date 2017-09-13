17721d5d3257b8f8dcf63f9e819aa084e8689ddf
Noor21afeefy <sweetnora838@gmail.com>
2017-09-13 13:27:17 +0300

    hjhiu

--- app/src/main/java/com/mobarky/tasnem/tasnemtestgithub/MainActivity.java
+++ app/src/main/java/com/mobarky/tasnem/tasnemtestgithub/MainActivity.java
@@ -3,6 +3,8 @@ package com.mobarky.tasnem.tasnemtestgithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,4 +20,24 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnItmScreen1:
+                Toast.makeText(getBaseContext(),"Hello in page1",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mnItmScreen2:
+                Toast.makeText(getBaseContext(),"Hello in page2",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mnItmScreen3:
+                Toast.makeText(getBaseContext(),"Hello in page3",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.mnItmScreen4:
+                Toast.makeText(getBaseContext(),"Hello in page4",Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        return true;
+
+    }
 }
---
+++ app/src/main/java/com/mobarky/tasnem/tasnemtestgithub/Page5.java
@@ -0,0 +1,13 @@
+package com.mobarky.tasnem.tasnemtestgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page5);
+    }
+}
